88d46d45dc69a004a37504112c17b230764c33ae
Lóránt Pintér <lorant.pinter@gmail.com>
2021-03-28 23:12:01 +0200

    Add BigQuery publisher

---
+++ google-iot/src/js/index.js
@@ -0,0 +1,33 @@
+const {BigQuery} = require('@google-cloud/bigquery');
+const bigquery = new BigQuery();
+
+/**
+ * Cloud Function entry point, Cloud Pub/Sub trigger.
+ * Extracts the metrics data from payload and insert to BigQuery
+ * @param {Object} event The event payload.
+ * @param {object} context The event metadata.
+ */
+exports.galagonyaPublish = (event, context) => {
+  const pubsubMessage = event.data;
+  const deviceId = event.attributes.deviceId;
+  const objStr = Buffer.from(pubsubMessage, 'base64').toString();
+  const msgObj = JSON.parse(objStr);
+  const timestamp = BigQuery.timestamp(new Date());
+  let rows = [{
+    device_id: deviceId,
+    time: timestamp,
+    light: msgObj.light,
+    gate: msgObj.gate,
+    motorPosition: msgObj.motorPosition,
+    openSwitch: msgObj.openSwitch,
+    closedSwitch: msgObj.closedSwitch
+  }];
+  insertRowsAsStream(rows);
+};
+
+function insertRowsAsStream (rows) {
+  bigquery
+    .dataset(process.env.DATASET)
+    .table(process.env.TABLE)
+    .insert(rows);
+}
