2443517c4dfd649adcb2b8301facfa3fa8e234fa
Lóránt Pintér <lorant.pinter@gmail.com>
2021-03-31 17:42:15 +0200

    Add Loopable

---
+++ include/Loopable.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Arduino.h>
+
+class Loopable {
+public:
+    virtual void loop() = 0;
+};
+
+class TimedLoopable : Loopable {
+public:
+    void loop() override {
+        unsigned long currentTimeMillis = millis();
+        if (currentTimeMillis - previousLoopMillis > getPeriodInMillis()) {
+            previousLoopMillis = currentTimeMillis;
+            timedLoop();
+        }
+    }
+
+protected:
+    virtual void timedLoop() = 0;
+    virtual unsigned long getPeriodInMillis() = 0;
+
+private:
+    unsigned long previousLoopMillis = 0;
+};
