bb4f4b3f7ea5534017e60c5657081dde522158f6
Lóránt Pintér <lorant.pinter@gmail.com>
2021-03-31 16:32:10 +0200

    Retry WIFI connection

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,73 @@ Client& chooseMqttConnection() {
     }
 }
 
+void startWifi() {
+    if (!config.wifiSsid.isEmpty()) {
+        Serial.print("Using stored WIFI configuration to connect to ");
+        Serial.print(config.wifiSsid);
+        Serial.print("...");
+        WiFi.begin(config.wifiSsid.c_str(), config.wifiPassword.c_str());
+    } else {
+        Serial.print("WIFI is not configured, using SmartConfig...");
+        bool smartConfigBeginSuccess = WiFi.beginSmartConfig();
+        if (!smartConfigBeginSuccess) {
+            Serial.print(" unsuccessful");
+        }
+    }
+}
+
+bool awaitConnect() {
+    unsigned long startTime = millis();
+    while (true) {
+        wl_status_t status = WiFi.status();
+        switch (status) {
+            case WL_CONNECTED:
+                return true;
+            case WL_CONNECT_FAILED:
+                Serial.println("WIFI connection failed");
+                return false;
+            default:
+                break;
+        }
+        // TODO Make WIFI connection timeout configurable
+        if (millis() - startTime > 10 * 1000) {
+            Serial.println("WIFI connection timed out");
+            return false;
+        }
+        delay(500);
+        Serial.print(".");
+    }
+}
+
+void connectWifi() {
+    bool wifiModeSuccessful = WiFi.mode(WIFI_AP_STA);
+    if (!wifiModeSuccessful) {
+        Serial.println("WIFI mode unsuccessful");
+    }
+    delay(500);
+    int retries = 3;
+    while (true) {
+        startWifi();
+        if (awaitConnect()) {
+            break;
+        }
+        WiFi.stopSmartConfig();
+        if (--retries > 0) {
+            continue;
+        }
+        Serial.println("Failed to connect WIFI, restarting");
+        ESP.restart();
+    }
+
+    WiFi.softAPsetHostname("chickens");
+    Serial.print(" connected, IP address: ");
+    Serial.print(WiFi.localIP());
+    Serial.print(", hostname: ");
+    Serial.println(WiFi.softAPgetHostname());
+
+    ota.begin("chickens");
+}
+
 void setup() {
     Serial.begin(115200);
 
@@ -79,37 +146,7 @@ void setup() {
 
     config.begin();
 
-    bool wifiModeSuccessful = WiFi.mode(WIFI_AP_STA);
-    if (!wifiModeSuccessful) {
-        Serial.println("WIFI mode unsuccessful");
-    }
-    delay(500);
-    if (!config.wifiSsid.isEmpty()) {
-        Serial.print("Using stored WIFI configuration to connect to ");
-        Serial.print(config.wifiSsid);
-        Serial.print("...");
-        WiFi.begin(config.wifiSsid.c_str(), config.wifiPassword.c_str());
-    } else {
-        Serial.print("WIFI is not configured, using SmartConfig...");
-        bool smartConfigBeginSuccess = WiFi.beginSmartConfig();
-        if (!smartConfigBeginSuccess) {
-            Serial.print(" unsuccessful");
-        }
-    }
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-        // Serial.print(WiFi.smartConfigDone());
-        // Serial.print(" ");
-        // Serial.println(WiFi.status());
-    }
-    WiFi.softAPsetHostname("chickens");
-    Serial.print(" connected, IP address: ");
-    Serial.print(WiFi.localIP());
-    Serial.print(", hostname: ");
-    Serial.println(WiFi.softAPgetHostname());
-
-    ota.begin("chickens");
+    connectWifi();
 
     File iotConfigFile = SPIFFS.open("/iot-config.json", FILE_READ);
     DynamicJsonDocument iotConfigJson(iotConfigFile.size() * 2);
