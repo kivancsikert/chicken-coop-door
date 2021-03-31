a83da3b6b5eb451535568697c4b14b572a28aa5f
Lóránt Pintér <lorant.pinter@gmail.com>
2021-03-31 19:11:25 +0200

    Move pin allocations to separate header file

---
+++ include/PinAllocation.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifdef ESP32
+
+#define MOTOR_PIN1 GPIO_NUM_23
+#define MOTOR_PIN2 GPIO_NUM_19
+#define MOTOR_PIN3 GPIO_NUM_18
+#define MOTOR_PIN4 GPIO_NUM_5
+
+#define LIGHT_SDA GPIO_NUM_15
+#define LIGHT_SCL GPIO_NUM_13
+
+#define OPEN_PIN GPIO_NUM_14
+#define CLOSED_PIN GPIO_NUM_12
+
+#elif defined(ESP8266)
+
+#define OPEN_PIN D5
+#define CLOSED_PIN D6
+
+#define MOTOR_PIN1 D0
+#define MOTOR_PIN2 D1
+#define MOTOR_PIN3 D2
+#define MOTOR_PIN4 D3
+
+#define LIGHT_SDA D7
+#define LIGHT_SDC D4
+
+#endif
