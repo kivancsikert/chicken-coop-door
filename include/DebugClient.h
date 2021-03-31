34526830a84f026ddf760054684034df3d02c95f
Lóránt Pintér <lorant.pinter@gmail.com>
2021-03-31 15:44:31 +0200

    Enable/disable logging for DebugClient

--- include/DebugClient.h
+++ include/DebugClient.h
@@ -14,106 +14,159 @@ public:
         this->delegate = delegate;
     }
 
+    void setLog(bool log) {
+        this->log = log;
+    }
+
     int connect(IPAddress ip, uint16_t port) {
-        Serial.print("> connect(");
-        Serial.print(ip);
-        Serial.print(", ");
-        Serial.print(port);
-        Serial.println();
+        if (log) {
+            Serial.print("> connect(");
+            Serial.print(ip);
+            Serial.print(", ");
+            Serial.print(port);
+            Serial.println();
+        }
         int result = delegate->connect(ip, port);
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
     int connect(const char* host, uint16_t port) {
-        Serial.print("> connect(");
-        Serial.print(host);
-        Serial.print(", ");
-        Serial.print(port);
-        Serial.println();
+        if (log) {
+            Serial.print("> connect(");
+            Serial.print(host);
+            Serial.print(", ");
+            Serial.print(port);
+            Serial.println();
+        }
         int result = delegate->connect(host, port);
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
     size_t write(uint8_t data) {
-        Serial.printf("> write(%u)\n", data);
+        if (log) {
+            Serial.printf("> write(%u)\n", data);
+        }
         size_t result = delegate->write(data);
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
     size_t write(const uint8_t* buf, size_t size) {
-        Serial.printf("> write([%u])\n", size);
+        if (log) {
+            Serial.printf("> write([%u])\n", size);
+        }
         size_t result = delegate->write(buf, size);
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
     int available() {
-        // Serial.println("> available()");
+        // if (log) {
+        //     Serial.println("> available()");
+        // }
         int result = delegate->available();
-        // Serial.print("< ");
-        // Serial.println(result);
+        // if (log) {
+        //     Serial.print("< ");
+        //     Serial.println(result);
+        // }
         return result;
     }
 
     int read() {
-        Serial.println("> read()");
+        if (log) {
+            Serial.println("> read()");
+        }
         int result = delegate->read();
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
     int read(uint8_t* buf, size_t size) {
-        Serial.printf("> read([%u])\n", size);
+        if (log) {
+            Serial.printf("> read([%u])\n", size);
+        }
         size_t result = delegate->read(buf, size);
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
     int peek() {
-        Serial.println("> peek()");
+        if (log) {
+            Serial.println("> peek()");
+        }
         int result = delegate->peek();
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
     void flush() {
-        Serial.println("> flush()");
+        if (log) {
+            Serial.println("> flush()");
+        }
         delegate->flush();
-        Serial.println("< (void)");
+        if (log) {
+            Serial.println("< (void)");
+        }
     }
 
     void stop() {
-        Serial.println("> stop()");
+        if (log) {
+            Serial.println("> stop()");
+        }
         delegate->stop();
-        Serial.println("< (void)");
+        if (log) {
+            Serial.println("< (void)");
+        }
     }
 
     uint8_t connected() {
-        Serial.println("> connected()");
+        if (log) {
+            Serial.println("> connected()");
+        }
         uint8_t result = delegate->connected();
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
     operator bool() {
-        Serial.println("> bool()");
+        if (log) {
+            Serial.println("> bool()");
+        }
         bool result = (bool) delegate;
-        Serial.print("< ");
-        Serial.println(result);
+        if (log) {
+            Serial.print("< ");
+            Serial.println(result);
+        }
         return result;
     }
 
 private:
     Client* delegate;
+    bool log = true;
 };
